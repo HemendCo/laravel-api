7775f3c40adbd20d4489f5ced8c9f3d9c5aa6625
balal <arastbalal@gmail.com>
2023-11-12 17:41:34 +0330

    Compatible with php 8.2

--- models/AclRoles.php
+++ models/AclRoles.php
@@ -2,31 +2,34 @@
 
 namespace App\Models;
 
+use Spatie\Permission\Contracts\Role as RoleContract;
 use Spatie\Permission\Models\Role as SpatieRole;
-use Spatie\Permission\PermissionRegistrar;
 
 class AclRoles extends SpatieRole
 {
-    protected $hidden = ['guard_name'];
+  protected $hidden = ['guard_name'];
 
-    protected static function findByParam(array $params = [])
-    {
-        $query = static::query();
+  protected static function findByParam(array $params = []): ?RoleContract
+  {
+    $query = static::query();
 
-        if (PermissionRegistrar::$teams) {
-            $query->where(function ($q) use ($params) {
-                $q->whereNull(PermissionRegistrar::$teamsKey)
-                    ->orWhere(PermissionRegistrar::$teamsKey, $params[PermissionRegistrar::$teamsKey] ?? getPermissionsTeamId());
-            });
-            unset($params[PermissionRegistrar::$teamsKey]);
-        }
+    $teams = config('permission.teams', false);
+    $teamsKey = config('permission.column_names.team_foreign_key', 'team_id');
 
-        $query->where('activated', '1');
-
-        foreach ($params as $key => $value) {
-            $query->where($key, $value);
-        }
-
-        return $query->first();
+    if ($teams) {
+      $query->where(function ($q) use ($params, $teamsKey) {
+        $q->whereNull($teamsKey)
+          ->orWhere($teamsKey, $params[$teamsKey] ?? getPermissionsTeamId());
+      });
+      unset($params[$teamsKey]);
     }
+
+    $query->where('activated', '1');
+
+    foreach ($params as $key => $value) {
+      $query->where($key, $value);
+    }
+
+    return $query->first();
+  }
 }
