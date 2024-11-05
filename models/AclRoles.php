773a4b4fc445865d17ba014fa73829687ec7c269
balalarast <arastbalal@gmail.com>
2024-11-05 06:00:25 +0330

    The findByParam method was moved from the AclRoles model to AclHandler

--- models/AclRoles.php
+++ models/AclRoles.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Hemend\Api\Traits\AclHandler;
-use Spatie\Permission\Contracts\Role as RoleContract;
 use Spatie\Permission\Models\Role as SpatieRole;
 
 class AclRoles extends SpatieRole
@@ -25,30 +24,5 @@ class AclRoles extends SpatieRole
       'activated' => 'integer',
       'is_default' => 'integer',
     ];
-  }
-
-  protected static function findByParam(array $params = []): ?RoleContract
-  {
-    $query = static::query();
-
-    $teams = config('permission.teams', false);
-    $teamsKey = config('permission.column_names.team_foreign_key', 'team_id');
-
-    if ($teams) {
-      $query->where(function ($q) use ($params, $teamsKey) {
-        $q->whereNull($teamsKey)
-          ->orWhere($teamsKey, $params[$teamsKey] ?? getPermissionsTeamId());
-      });
-      unset($params[$teamsKey]);
-    }
-
-    $query->where('activated', '1');
-    $query->where('not_deleted', '1');
-
-    foreach ($params as $key => $value) {
-      $query->where($key, $value);
-    }
-
-    return $query->first();
   }
 }
--- src/Traits/AclHandler.php
+++ src/Traits/AclHandler.php
@@ -2,7 +2,34 @@
 
 namespace Hemend\Api\Traits;
 
+use Spatie\Permission\Contracts\Role as RoleContract;
+
 trait AclHandler
 {
   protected string $guard_name = '*';
+
+  protected static function findByParam(array $params = []): ?RoleContract
+  {
+    $query = static::query();
+
+    $teams = config('permission.teams', false);
+    $teamsKey = config('permission.column_names.team_foreign_key', 'team_id');
+
+    if ($teams) {
+      $query->where(function ($q) use ($params, $teamsKey) {
+        $q->whereNull($teamsKey)
+          ->orWhere($teamsKey, $params[$teamsKey] ?? getPermissionsTeamId());
+      });
+      unset($params[$teamsKey]);
+    }
+
+    $query->where('activated', '1');
+    $query->where('not_deleted', '1');
+
+    foreach ($params as $key => $value) {
+      $query->where($key, $value);
+    }
+
+    return $query->first();
+  }
 }
